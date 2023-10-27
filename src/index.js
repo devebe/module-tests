b0d7b4244bcde20a6aca6a96cf1b87a157119ffb
devebe <dvbirdja@gmail.com>
2023-10-28 00:25:28 +0200

    Use webpack to load image

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import add from "./calc";
 import './style.css';
+import Skepta from './skepta.jpg';
 
 console.log(add(5,4));
+
+const body = document.querySelector('body');
+body.textContent = "Meridian Man";
+
+const element = document.createElement('div');
+const myImage = new Image();
+myImage.src = Skepta;
+
+body.appendChild(element);
+element.appendChild(myImage);
